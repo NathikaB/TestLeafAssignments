48d529ff3eabc8325f11515d9c96dfe17b534c81
NathikaB <nathika.balamurugan@gmail.com>
2026-07-08 08:39:55 +0530

    Add files via upload
    Day2

---
+++ week1.day2/Variable.js
@@ -0,0 +1,19 @@
+let userName="Nathika"
+console.log(userName);
+console.log(typeof userName);
+
+let organization="TCS"
+console.log(organization);
+console.log(typeof organization);
+
+let contactNumer=987654320
+console.log(contactNumer);
+console.log(typeof contactNumer);
+
+let knowsAutomation=true
+console.log(knowsAutomation);
+console.log(typeof knowsAutomation);
+
+let usesPlaywrite
+console.log(usesPlaywrite);
+console.log(typeof usesPlaywrite);
